726763040dbd10386d491c090eb888e09a519483
codeminnam <code.minnam@gmail.com>
2020-05-17 02:09:09 +0900

    #5.4 requestSecret Mutation and Refactor
    #5.4 requestSecret Mutation and Refactor

--- src/Routes/Auth/AuthPresenter.js
+++ src/Routes/Auth/AuthPresenter.js
@@ -1,8 +1,7 @@
-import React, {useState} from "react";
+import React from "react";
 import styled from "styled-components";
-import Input from "../Components/Input";
-import Button from "../Components/Button";
-import useInput from "../Hooks/useInput";
+import Input from "../../Components/Input";
+import Button from "../../Components/Button";
 
 const Wrapper = styled.div`
     min-height: 80vh;
@@ -48,29 +47,29 @@ const Form = styled(Box)`
 `;
 
 
-export default () => {
-    const [action, setAction] = useState("logIn");
-    const username = useInput("");
-    const password = useInput("");
-    const firstName = useInput("");
-    const lastName = useInput("");
-    const email = useInput("");
+export default ({
+    action,
+    setAction,
+    username,
+    firstName,
+    lastName,
+    email,
+    onLogin
+}) => {
     return (
     <Wrapper>
         <Form>
             {action === "logIn"? (
                 <form>
-                    <Input placeholder={"Username"} {...username}/>
-                    <Input placeholder={"Password"} {...password} type="password"/>
+                    <Input placeholder={"Email"} {...email} type="email" onSubmit={onLogin}/>
                     <Button text="Log In"/>
                 </form>
             ):(
                 <form>
                     <Input placeholder={"First name"} {...firstName}/>
                     <Input placeholder={"Last name"} {...lastName}/>
-                    <Input placeholder={"Email"} {...email} type="email"/>
+                    <Input placeholder={"Email"} {...email} type="email" onSubmit={onLogin}/>
                     <Input placeholder={"Username"} {...username}/>
-                    <Input placeholder={"Password"} {...password} type="password"/>
                     <Button text="Sign Up"/>
                 </form>
             )}
